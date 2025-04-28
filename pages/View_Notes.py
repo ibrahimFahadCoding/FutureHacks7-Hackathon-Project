5fcfe02f905c1d4c558856a51f693ad09ff1870e
Ibrahim Fahad <ibrahimthecoder123@gmail.com>
2025-04-28 16:16:46 -0700

    hi

--- pages/View_Notes.py
+++ pages/View_Notes.py
@@ -9,20 +9,23 @@ st.title("📚 Saved Notes")
 
 
 #Get Credentials
-username = st.session_state.get("username", "guest")
-summaries = load_summaries().get(username, [])
+username = st.session_state.get("username")
 
-if not summaries:
-    st.info("No Notes Saved Yet.")
+if username:
+    summaries = load_summaries().get(username, [])
+
+    if not summaries:
+        st.info("No Notes Saved Yet.")
+    else:
+        for idx, s in enumerate(summaries):
+            with st.expander(f"{s['title']}"):
+                st.markdown(s["summary"])
+                if st.button("Delete", key=f"del_{idx}"):
+                    summaries.pop(idx)
+                    st.success("Summary Deleted!")
+                    os.makedirs("data", exist_ok=True)
+                    with open("data/summaries.json", "w") as f:
+                        json.dump({username: summaries}, f, indent=2)
+                    st.rerun()
 else:
-    for idx, s in enumerate(summaries):
-        with st.expander(f"{s['title']}"):
-            st.markdown(s["summary"])
-            if st.button("Delete", key=f"del_{idx}"):
-                summaries.pop(idx)
-                st.success("Summary Deleted!")
-                os.makedirs("data", exist_ok=True)
-                with open("data/summaries.json", "w") as f:
-                    json.dump({username: summaries}, f, indent=2)
-                st.rerun()
-
+    st.warning("Please Log In to Continue")
